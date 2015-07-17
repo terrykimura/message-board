d30a55b7902492efcb5cff2838651697d72bec9c
terrykimura <terry.kimura@gmail.com>
2015-07-17 11:50:09 +0000

    Implementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    # Get all messages
     @messages = Message.all
+    @message = Message.new
   end
   
   ## Terry wuz here
   
   def create
     @message = Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'Your message has been saved'
+    if @message.save
+      redirect_to root_path , notice: 'Your message has been saved'
+    else
+      #If message was not saved
+      @messages = Message.all
+      flash.now[:alert] = "A poopoo happened upon attempting to save your message"
+      render 'index'
+    end
   end
   
   private
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ActiveRecord::Base
+    # Name is required; under 20 chars
+    validates :name , length: { maximum: 20 } , presence: true
+    # Message is required; between 2 and 30 chars
+    validates :body , length: {minimum: 2 , maximum: 30 } , presence: true
 end
